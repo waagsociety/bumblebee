a742516f24deb7db5a8484804c7ed2c08bd5801d
Ivo de Kler <ivo.de.kler@gmail.com>
2015-10-22 17:14:57 +0200

    fixed parameter order

--- transformers/returnPropertyFromObject.js
+++ transformers/returnPropertyFromObject.js
@@ -1,4 +1,4 @@
-module.exports.transform = function returnPropertyFromObject(data, context, propertyName){
+module.exports.transform = function returnPropertyFromObject(context, data, propertyName){
 	if( !data || !data[propertyName] ) return new Error('returnPropertyFromObject: no data or no data.' + propertyName + ' passed, data: ' + JSON.stringify( data ) );
 
 	return data[propertyName];
