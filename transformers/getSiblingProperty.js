1da95d56720e63ee5ce64025d1af11df57bb6d01
Ivo de Kler <ivo.de.kler@gmail.com>
2015-10-23 14:12:14 +0200

    removed console.log

--- transformers/getSiblingProperty.js
+++ transformers/getSiblingProperty.js
@@ -1,7 +1,6 @@
 // todo: allow nested properties to be accessed
 
 module.exports.transform = function getSiblingProperty( context, data, argumentString ) {
-	console.log(context);
 	var split = argumentString.split(','),
 			entityName = split.shift(),
 			propertyName = split.shift(),
