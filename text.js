52e590d6736ec644be4bce7f1771472526aa1f20
Ivo de Kler <ivo.de.kler@gmail.com>
2015-11-03 12:21:12 +0100

    added texts file

---
+++ text.js
@@ -0,0 +1,5 @@
+module.exports = {
+	dataset: {
+		preTransformationExplanation: 'In order to proceed transforming this file, please select an appropriate mapping file and click \'transform\''
+	}
+};
