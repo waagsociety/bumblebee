a88c60afbafb6ad4ebafe8e0c9764579e313f700
Ivo de Kler <ivo.de.kler@gmail.com>
2015-11-03 17:00:18 +0100

    added some explanatory texts

--- text.js
+++ text.js
@@ -1,5 +1,11 @@
 module.exports = {
+	datasets: {
+		uploadMessage: 'Or upload another'
+	},
 	dataset: {
 		preTransformationExplanation: 'In order to proceed transforming this file, please select an appropriate mapping file and click \'transform\''
+	},
+	transform: {
+		topExplanation: 'Some items may not pass validation. Please correct the items that pop up below, and either approve them or reject them'
 	}
 };
