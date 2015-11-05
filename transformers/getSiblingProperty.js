6d35431e43b49bf04753d8e39177eea4678133b8
Ivo de Kler <ivo.de.kler@gmail.com>
2015-11-05 13:42:42 +0100

    changed argument notation for getSiblingProperty

--- transformers/getSiblingProperty.js
+++ transformers/getSiblingProperty.js
@@ -1,9 +1,15 @@
 // todo: allow nested properties to be accessed
 
 module.exports.transform = function getSiblingProperty( context, data, argumentString ) {
-	var split = argumentString.split(','),
-			entityName = split.shift(),
-			propertyName = split.shift(),
+	var split = argumentString.split('.'),
+			entityName = split.shift();
+
+	// maybe the entityName contained an escaped dot and needs to be expanded
+	while( entityName[ entityName.length - 1] === '\\' ){
+		entityName = entityName.slice( 0, entityName.length - 1 ) + '.' + split.shift();
+	}
+
+	var propertyName = split.shift(),
 			sibling = context.currentEntities[entityName],
 			value = sibling && sibling[propertyName];
 
