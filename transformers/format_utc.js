3404577a4a42c8e96086c3688f26e37ef70897c4
Ivo de Kler <ivo.de.kler@gmail.com>
2015-11-09 13:56:11 +0100

    fixed format_utc -> crashed on invalid date

--- transformers/format_utc.js
+++ transformers/format_utc.js
@@ -1,6 +1,6 @@
 //format a date value as utc
 exports.transform = function (context, data) {
-	if( data && data instanceof Date ) return data.toISOString();
+	if( data && data instanceof Date && data.toString() !== 'Invalid Date' ) return data.toISOString();
 
 	return new Error( 'format_utc: data is not a Date, data: ' + JSON.stringify( data ) );
 };
